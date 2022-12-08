ee436ad284e32c2dd3c962dd9061539ffb8b33b8
EniseK <enisekizilaslan@gmail.com>
2022-12-08 13:00:25 +0100

    Added functionality to the select menu of cities to get live date and time according to the time zones.

--- notes.js
+++ notes.js
@@ -1,4 +1,8 @@
-function updateTime() {
+moment()
+	.tz("Asia/Istanbul")
+	.format("[<strong> Gaziantep, TR:</strong>] dddd, MMMM D, YYYY hh:mm:ss A ");
+
+setInterval(function () {
 	let istanbulElement = document.querySelector("#istanbul");
 	let istanbulDate = istanbulElement.querySelector(".date");
 	let istanbulTime = istanbulElement.querySelector(".time");
@@ -10,7 +14,9 @@ function updateTime() {
 	istanbulTime.innerHTML = moment()
 		.tz("Europe/Istanbul")
 		.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
 
+setInterval(function () {
 	let seattleElement = document.querySelector("#seattle");
 	let seattleDate = seattleElement.querySelector(".date");
 	let seattleTime = seattleElement.querySelector(".time");
@@ -22,22 +28,20 @@ function updateTime() {
 	seattleTime.innerHTML = moment()
 		.tz("America/Vancouver")
 		.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
 
-	function displayTime(event) {
-		let displayElement = document.querySelector(".display");
-		let location = event.target.value;
-		let cityName = location.replace("-", " ").split("/")[1];
-		let time = moment().tz(location);
+function displayTime(event) {
+	let displayElement = document.querySelector(".display");
+	let location = event.target.value;
+	let cityName = location.replace("-", " ").split("/")[1];
+	let time = moment().tz(location);
 
-		displayElement.innerHTML = `<div class="city"><div>
+	displayElement.innerHTML = `<div class="city"><div>
 						<h2>${cityName}</h2>
 						<div class="date">${time.format("MMMM Do YYYY")}</div>
 					</div>
 					<div class="time">${time.format("h:mm:ss [<small>]A[</small>]")}</div></div>`;
-	}
-
-	let city = document.querySelector("#cityMenu");
-	city.addEventListener("change", displayTime);
 }
 
-setInterval(updateTime, 1000);
+let city = document.querySelector("#cityMenu");
+city.addEventListener("change", displayTime);
